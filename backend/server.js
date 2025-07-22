bb70633041899aad8f3ebb41bcd764daef1950d9
Harish Dewangan <84856962+officialharis@users.noreply.github.com>
2025-07-22 11:23:56 +0000

    update in server.js

--- backend/server.js
+++ backend/server.js
@@ -9,37 +9,41 @@ import productRouter from './routes/productRoute.js';
 import cartRouter from './routes/cartRoute.js';
 import orderRouter from './routes/orderRoute.js';
 
-
-
 const app = express();
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000;
 
+const startServer = async () => {
+  try {
+    await connectDB();
+    connectCloudinary(); 
 
-connectDB().catch(err => {
-    console.log("Failed to connect to MongoDB:", err.message);
-});
-connectCloudinary();
-
-app.use(express.json());
-app.use(cors( { origin: [
+    app.use(express.json());
+    app.use(cors({
+      origin: [
         'http://localhost:5173',
         'http://localhost:5174',
         'https://forever-ecom-frontend-sand.vercel.app',
         'https://forever-admin-three-jet.vercel.app',
-    ],
-    credentials: true
-}));
+      ],
+      credentials: true
+    }));
 
+    app.use('/api/user', userRouter);
+    app.use('/api/product', productRouter);
+    app.use('/api/cart', cartRouter);
+    app.use('/api/order', orderRouter);
 
-app.use('/api/user', userRouter)
-app.use('/api/product', productRouter)
-app.use('/api/cart', cartRouter)
-app.use('/api/order', orderRouter)
+    app.get('/', (req, res) => {
+      res.send('API Working');
+    });
 
-app.get('/',(req,res)=>{
-    res.send('API Working')
-    
-})
+    app.listen(port, () => {
+      console.log(`🚀 Server started on PORT: ${port}`);
+    });
 
+  } catch (err) {
+    console.error("Failed to start server:", err.message);
+  }
+};
 
-app.listen(port, ()=> console.log('server started on PORT : '+ port));
+startServer();
